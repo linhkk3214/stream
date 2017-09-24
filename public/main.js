d5a66b88b10cb4f39b33f7d9d5c2a6d4963c2b4b
linhkk3214 <32235851+linhkk3214@users.noreply.github.com>
2017-09-24 16:23:35 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -7,5 +7,28 @@ function playStream(IdTagVideo, stream) {
     $video.srcObject = stream;
     $video.play();
 }
-openStream()
-    .then(stream => playStream("localStream", stream));
+//openStream()
+//    .then(stream => playStream("localStream", stream));
+
+const peer = new Peer({ "key": "linhdeptraovqbd" });
+peer.on("open", id => {
+    console.log(1);
+    $("#txtLocalIdPeer").html(id);
+});
+$("#btnCall").click(function () {
+    var remoteId = $("#txtRemoteId").val();
+    openStream()
+        .then(stream => {
+            playStream("localStream", stream);
+            const call = peer.call(remoteId, stream);
+            call.on("stream", stream => playStream("remoteStream", stream))
+        });
+});
+peer.on("call", (call) => {
+    openStream()
+        .then(stream => {
+            call.answer(stream);
+            playStream("localStream", stream);
+            playStream.on("stream", (stream) => play("remoteStream", stream));
+        });
+});
