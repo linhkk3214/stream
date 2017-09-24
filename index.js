b4661cce76c1174a97df48c620f584ac30ba3f97
linhkk3214 <32235851+linhkk3214@users.noreply.github.com>
2017-09-24 18:21:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get("/auth/fb/cb", passport.authenticate("facebook", {
     successRedirect: "/success",
 }) );
 
-server.listen(port, () => { console.log(port); console.log("server da hoat dong") });
+server.listen(port, () => { console.log("server da hoat dong") });
 
 passport.use(new passportfb(
     {
@@ -84,4 +84,35 @@ passport.deserializeUser((id, done) => {
     db.findOne({ id: id }, (err, user) => {
         done(null, user);
     });
+});
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+function playStream(IdTagVideo, stream) {
+    var $video = document.getElementById(IdTagVideo);
+    $video.srcObject = stream;
+    $video.play();
+}
+const peer = new Peer({ "key": "tr3izjcg20jatt9", "host": "stream6969.herokuapp.com", secure: true, port: 443 });
+peer.on("open", () => {
+    console.log(1);
+    $("#txtLocalIdPeer").html(peer.id);
+});
+$("#btnCall").click(function () {
+    var remoteId = $("#txtRemoteId").val();
+    openStream()
+        .then(stream => {
+            playStream("localStream", stream);
+            const call = peer.call(remoteId, stream);
+            call.on("stream", stream => playStream("remoteStream", stream))
+        });
+});
+peer.on("call", (call) => {
+    openStream()
+        .then(stream => {
+            call.answer(stream);
+            playStream("localStream", stream);
+            playStream.on("stream", (stream) => play("remoteStream", stream));
+        });
 });
